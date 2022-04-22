757e4764cb2dd6e6c831e14ad3c7b985f14f919d
Maschell <Maschell@gmx.de>
2022-04-22 21:59:17 +0200

    Improve logging and add logging section to the README

--- Makefile
+++ Makefile
@@ -44,6 +44,11 @@ CXXFLAGS += -DDEBUG -g
 CFLAGS += -DDEBUG -g
 endif
 
+ifeq ($(DEBUG),VERBOSE)
+CXXFLAGS += -DDEBUG -DVERBOSE_DEBUG -g
+CFLAGS += -DDEBUG -DVERBOSE_DEBUG -g
+endif
+
 LIBS	:= -lwums -lwut -lfunctionpatcher
 
 #-------------------------------------------------------------------------------
--- src/utils/logger.h
+++ src/utils/logger.h
@@ -11,8 +11,6 @@ extern "C" {
 #define __FILENAME_X__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 #define __FILENAME__   (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILENAME_X__)
 
-// #define VERBOSE_DEBUG
-
 #ifdef DEBUG
 
 #ifdef VERBOSE_DEBUG
@@ -30,6 +28,7 @@ extern "C" {
 #define DEBUG_FUNCTION_LINE_VERBOSE(FMT, ARGS...)                              while (0)
 
 #define DEBUG_FUNCTION_LINE_VERBOSE_EX(FILENAME, FUNCTION, LINE, FMT, ARGS...) while (0)
+
 #endif
 
 #define DEBUG_FUNCTION_LINE(FMT, ARGS...)                                                        \
@@ -42,6 +41,16 @@ extern "C" {
         WHBLogWritef("[%23s]%30s@L%04d: " FMT "", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
     } while (0)
 
+#define DEBUG_FUNCTION_LINE_ERR(FMT, ARGS...)                                                                \
+    do {                                                                                                     \
+        WHBLogPrintf("[%23s]%30s@L%04d: ##ERROR## " FMT "\n", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
+    } while (0)
+
+#define DEBUG_FUNCTION_LINE_ERR_LAMBDA(FILENAME, FUNCTION, LINE, FMT, ARGS...)                   \
+    do {                                                                                         \
+        WHBLogPrintf("[%23s]%30s@L%04d: ##ERROR## " FMT "\n", FILENAME, FUNCTION, LINE, ##ARGS); \
+    } while (0)
+
 #else
 
 #define DEBUG_FUNCTION_LINE_VERBOSE(FMT, ARGS...)                              while (0)
@@ -52,17 +61,18 @@ extern "C" {
 
 #define DEBUG_FUNCTION_LINE_WRITE(FMT, ARGS...)                                while (0)
 
-#endif
-
-#define DEBUG_FUNCTION_LINE_ERR(FMT, ARGS...)                                                                        \
-    do {                                                                                                             \
-        OSReport("## ERROR ## [%23s]%30s@L%04d: ##ERROR## " FMT "\n", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
+#define DEBUG_FUNCTION_LINE_ERR(FMT, ARGS...)                                                            \
+    do {                                                                                                 \
+        OSReport("[%23s]%30s@L%04d: ##ERROR## " FMT "\n", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
     } while (0)
 
 #define DEBUG_FUNCTION_LINE_ERR_LAMBDA(FILENAME, FUNCTION, LINE, FMT, ARGS...)               \
     do {                                                                                     \
         OSReport("[%23s]%30s@L%04d: ##ERROR## " FMT "\n", FILENAME, FUNCTION, LINE, ##ARGS); \
     } while (0)
+
+#endif
+
 
 void initLogging();
 
