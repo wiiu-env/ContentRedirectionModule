a0f933b1a36f470be91f40b63b40eb9658f0cf3a
Maschell <Maschell@gmx.de>
2022-10-04 18:40:26 +0200

    Prevent calling OSCancelThread for ContentRedirection IO Threads

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,22 @@
 #include "utils/StringTools.h"
 #include "utils/logger.h"
 #include "version.h"
-#include <coreinit/cache.h>
-#include <coreinit/core.h>
-#include <malloc.h>
 #include <wums.h>
 
 WUMS_MODULE_EXPORT_NAME("homebrew_content_redirection");
 WUMS_USE_WUT_DEVOPTAB();
 
 #define VERSION "v0.2.1"
+
+DECL_FUNCTION(void, OSCancelThread, OSThread *thread) {
+    if (thread == gThreadData[0].thread || thread == gThreadData[1].thread || thread == gThreadData[2].thread) {
+        DEBUG_FUNCTION_LINE_INFO("Prevent calling OSCancelThread for ContentRedirection IO Threads");
+        return;
+    }
+    real_OSCancelThread(thread);
+}
+
+function_replacement_data_t OSCancelThreadReplacement = REPLACE_FUNCTION(OSCancelThread, LIBRARY_COREINIT, OSCancelThread);
 
 WUMS_INITIALIZE() {
     initLogging();
@@ -27,7 +34,9 @@ WUMS_INITIALIZE() {
             OSFatal("homebrew_content_redirection: Failed to patch function");
         }
     }
-
+    if (!FunctionPatcherPatchFunction(&OSCancelThreadReplacement, nullptr)) {
+        OSFatal("homebrew_content_redirection: Failed to patch OSCancelThreadReplacement");
+    }
     DEBUG_FUNCTION_LINE("Patch functions finished");
     deinitLogging();
 }
